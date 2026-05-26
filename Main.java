f18b4ce39084cf1ac62a27d8ed6bcb0dc0606e06
Sandeep Kumar Tiwari <sandeepkumart088@gmail.com>
2026-05-27 01:21:55 +0530

    Add CSES solutions

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[]arr = new int[n-1];
+        for(int i = 0; i<arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        long sum = 0;
+        for(int i = 1; i<=n; i++){
+            sum += i;
+        }
+        long actualsum = 0;
+        for(int i= 0; i<arr.length; i++){
+            actualsum += arr[i];
+        }
+        System.out.println(sum - actualsum);
+   
+    }
+}
